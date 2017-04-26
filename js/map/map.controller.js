3c187f4db655e78e813d4c52fd35e0629e36fb30
Jeezou <Jeezou>
2017-04-26 23:59:34 +0200

    added openstreetmap layer to see color on mal; added nests markers

--- js/map/map.controller.js
+++ js/map/map.controller.js
@@ -5,14 +5,24 @@ angular.module('app.map', ['firebase'])
 // Home controller
   .controller('MapController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray){
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoidmFscmVmZ2VvIiwiYSI6ImNpdndlZDFyNTAxNGoyenBkZ3d1Z2phZmMifQ.nzDRmWqsETzMwJyIwrNazQ';
+    L.mapbox.accessToken = 'pk.eyJ1IjoidmFscmVmZ2VvIiwiYSI6ImNpdndlZDFyNTAxNGoyenBkZ3d1Z2phZmMifQ.nzDRmWqsETzMwJyIwrNazQ';
+    //mapboxgl.accessToken = 'pk.eyJ1IjoidmFscmVmZ2VvIiwiYSI6ImNpdndlZDFyNTAxNGoyenBkZ3d1Z2phZmMifQ.nzDRmWqsETzMwJyIwrNazQ';
+
+    var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=' + L.mapbox.accessToken, {
+      attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    var mapLeaflet = L.mapbox.map('map-leaflet', 'mapbox.light')
+      .addLayer(mapboxTiles)
+      .setView([46.9896, 3.159 ], 5);
+/*
     var map = new mapboxgl.Map({
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
       center: [3.159, 46.9896], // starting position
       zoom: 5 // starting zoom
     });
-
+*/
 
     console.log("MapController");
     var ref = firebase.database().ref();
@@ -21,17 +31,11 @@ angular.module('app.map', ['firebase'])
       .then(function(){
         angular.forEach($scope.nests, function(user) {
           angular.forEach(user, (function(nest) {
-            console.log(nest['diameter']);
+            if (nest && nest['diameter']){
+              L.marker([nest['lat'], nest['long']]).addTo(mapLeaflet);
+            }
           }));
         })
       });
-    /*for (var nest in $scope.nests[$scope.nests.size - 1]){
-      console.log(nest);
 
-    }*/
-
-    console.log($scope.nests);
-    //var vm = this;
-
-    //console.log($firebase(new Firebase("https://infonid-e83a2.firebaseio.com" + '/nests')));
   }]);
